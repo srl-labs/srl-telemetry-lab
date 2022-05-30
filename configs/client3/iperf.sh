cd7b9b09be6a27c0782c90913fede6a0df2153d5
bclasse <claeys.bastien@skynet.be>
2022-05-30 14:36:11 +0200

    Fix iperf description

--- configs/client3/iperf.sh
+++ configs/client3/iperf.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Start iperf3 server in the background
-# with 4 parallel tcp streams, each 200 Kbit/s == 800Kbit/s
+# with 32 parallel tcp streams, each 200 Kbit/s == 6.4Mbit/s
 # using ipv6 interfaces
 iperf3 -c 2002::172:17:0:1 -t 10000 -i 1 -p 5202 -B 2002::172:17:0:3 -P 32 -b 200K -M 1460 &
