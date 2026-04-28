9183e6745d4bb2dfb5ea88b915fcbc1ab4279a3b
FloSch62 <schwarz.flori.88@gmail.com>
2026-04-28 14:14:57 +0200

    bump srl to 25.10

--- configs/client2/iperf.sh
+++ configs/client2/iperf.sh
@@ -5,4 +5,4 @@
 # Start iperf3 server in the background
 # with 8 parallel tcp streams, each 200 Kbit/s == 1.6Mbit/s
 # using ipv6 interfaces
-iperf3 -c 2002::172:17:0:1 -t 10000 -i 1 -p 5201 -B 2002::172:17:0:2 -P 8 -b 200K -M 1460 &
+iperf3 -c 2002::172:17:0:1 -t 10000 -i 1 -p 5201 -B 2002::172:17:0:2 -P 8 -b 200K -M 1480 -l 1480 &
